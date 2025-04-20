1f711f758c73742c6738e13966350f7d1f11feec
snfy <139520993+lovejjw@users.noreply.github.com>
2025-04-20 23:19:19 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    // 配置路径别名
+    alias: {
+      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+      "~": path.resolve("./src")
+    }
+  },
+})
